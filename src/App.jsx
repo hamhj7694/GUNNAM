9f2e8d769fc9493fe6d56e924db2ea0a43585145
hamhj7694 <hamhj7694@gmail.com>
2026-07-10 15:56:29 +0900

    건넴 완성.v1

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/gunnam">
       <Routes>
         <Route path="/" element={<HomePage {...pageProps} />} />
         <Route path="/create" element={<CreateCardPage {...pageProps} />} />
